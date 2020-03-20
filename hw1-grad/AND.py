a86543da6f8f203bedab30757a100a329ac82793
rolf0504 <fangrolf@gmail.com>
2020-03-21 01:00:40 +0800

    1

--- hw1-grad/AND.py
+++ hw1-grad/AND.py
@@ -1,25 +1,3 @@
-'''
-AND Gate
- x | y | o 
- 0 | 0 | 0
- 0 | 1 | 0
- 1 | 0 | 0
- 1 | 1 | 1
-
- 0*w1 + 0*w2 + b = 0
- 0*w1 + 1*w2 + b = 0
- 1*w1 + 0*w2 + b = 0
- 1*w1 + 1*w2 + b = 1
-
-A X = B
-
- A = 0 0 1  X = w1  B = 0
-     0 1 1      w2      0
-     1 0 1      b       0
-     1 1 1              1
-
- x*w1 + y*w2 + b = o
-'''
 import gd2 as gd
 import numpy as np
 from numpy.linalg import norm
