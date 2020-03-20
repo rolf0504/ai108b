1a117c63e1ab2d0e3187709ec0294c875f11aa3b
rolf0504 <fangrolf@gmail.com>
2020-03-21 01:05:16 +0800

    1

--- hw1-grad/AND2.py
+++ hw1-grad/AND2.py
@@ -1,23 +1,36 @@
+import gd2 as gd
 import numpy as np
 from numpy.linalg import norm
     
-A = np.array([[0.0, 0], 
-             [0, 1], 
-             [1, 0], 
-             [1, 1]])
-B = np.array([0.0, 0, 0, 1]).transpose()
+A = np.array([[0, 1, 1], 
+             [1, 0, 1], 
+             [1, 1, 1]])
+B = np.array([0.0, 0, 1]).transpose()
 
 def sig(x):
     s = 1 / (1 + np.exp(-x))
     return s
 
-def f(w, b):
-    X = w
+def f(p):
+    X = p
     Y = np.dot(A, X)
-    print(Y)
-    print(Y + b)
-    return norm(Y + b)
+    return norm(Y - B)
 
-w = np.array([1.0, 1])
-b = -1
-print(f(w, b))
+p = np.array([0.0, 0, 0])
+p = gd.gradientDescendent(f, p)
+
+A = np.array([[0, 0, 1], 
+             [0, 1, 1],
+             [1, 0, 1], 
+             [1, 1, 1]])
+B = np.dot(A, p)
+
+print("p = {}".format(p))
+print("A = {}".format(A))
+print("A dot p = {}".format(B))
+print("sig(B) = {}".format(sig(B)))
+ans = []
+for i in B:
+    ans.append(0) if sig(i) <= 0.5 else ans.append(1)
+    # ans.append(0) if sig(i) <= 0.6 else ans.append(1)
+print("ans = {}".format(ans))
