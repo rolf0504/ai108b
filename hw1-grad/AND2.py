ae930fff0b967d2a3a1bd08cba3c84e28407075e
rolf0504 <fangrolf@gmail.com>
2020-03-21 13:31:54 +0800

    1

--- hw1-grad/AND2.py
+++ hw1-grad/AND2.py
@@ -18,7 +18,6 @@ def f(p):
 
 p = np.array([0.0, 0, 0])
 p = gd.gradientDescendent(f, p)
-
 print("\n==================================================================================\n")
 
 print("p = {}".format(p))
@@ -28,25 +27,25 @@ inputArr = np.array([[0, 0, 1],
              [1, 0, 1], 
              [1, 1, 1]])
 B = np.dot(inputArr, p)
-
-
-
 print("\nInputs: \n{}".format(inputArr))
 print("A dot p = {}".format(B))
 print("sig(B) = {}".format(sig(B)))
+
 ans = []
 for i in B:
     ans.append(0) if sig(i) <= 0.5 else ans.append(1)
     # ans.append(0) if sig(i) <= 0.6 else ans.append(1)
 print("Output = {}".format(ans))
-
 print("\n==================================================================================\n")
+
 print("[w1, w2, b] = {}".format(p))
+
 X = p.copy()
 for i in range(len(p)):
     X[i] = round(p[i])
 print("\nRounded weights & bias: {}".format(X))
 print("--------------------------------")
+
 print("\tInputs  |  Outputs")
 for i in range(len(ans)):
     print("\t{}\t\t{}".format(inputArr[i], ans[i]))
