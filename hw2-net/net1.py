365bc18a06631dae794b052b74d72cdaa3d1212c
rolf0504 <fangrolf@gmail.com>
2020-03-26 12:02:53 +0800

    1

--- hw2-net/net1.py
+++ hw2-net/net1.py
@@ -4,9 +4,11 @@ net = Net()
 x = net.variable(1)
 y = net.variable(2)
 z = net.variable(3)
+
 x2 = net.mul(x, x)
 y2 = net.mul(y, y)
 z2 = net.mul(z, z)
+
 o1 = net.add(x2, y2)
 o = net.add(o1, z2)
 
