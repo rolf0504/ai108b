03e8afbaefe2e87eb081ac642ba50ea85f5da7e9
rolf0504 <fangrolf@gmail.com>
2020-04-30 11:28:15 +0800

    1

--- hw4-bullshit/main.py
+++ hw4-bullshit/main.py
@@ -1,11 +1,11 @@
 import os, re, json
 import random
 
-def readJSON(fileName=""):
+def readJSON(fileName = ""):
     if fileName!='':
         strList = fileName.split(".")
         if strList[len(strList)-1].lower() == "json":
-            with open(fileName,mode='r',encoding="utf-8") as file:
+            with open(fileName,mode='r', encoding="utf-8") as file:
                 return json.loads(file.read())
 
 data = readJSON("data.json")
@@ -32,8 +32,8 @@ nextFam = shuf(famous)
 def giveSentense():
     global nextFam
     xx = next(nextFam)
-    xx = xx.replace(  "a",random.choice(before) )
-    xx = xx.replace(  "b",random.choice(after) )
+    xx = xx.replace("a",random.choice(before))
+    xx = xx.replace("b",random.choice(after))
     return xx
 
 def another():
@@ -47,7 +47,7 @@ if __name__ == "__main__":
     length = input("Article length: ")
     for x in xx:
         tmp = str()
-        while ( len(tmp) < int(length)) :
+        while (len(tmp) < int(length)) :
             branch = random.randint(0,100)
             if branch < 5:
                 tmp += another()
@@ -55,5 +55,5 @@ if __name__ == "__main__":
                 tmp += giveSentense()
             else:
                 tmp += next(nextBosh)
-        tmp = tmp.replace("x",xx)
+        tmp = tmp.replace("x", xx)
         print(tmp)
